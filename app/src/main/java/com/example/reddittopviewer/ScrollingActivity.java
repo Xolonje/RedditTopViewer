46961b607f728099fe63376bb70b7529dba068ce
Asya Epshtein <73520096+Xolonje@users.noreply.github.com>
2023-02-05 21:40:32 +0200

    JSON array connected(Fixed)

--- app/src/main/java/com/example/reddittopviewer/ScrollingActivity.java
+++ app/src/main/java/com/example/reddittopviewer/ScrollingActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -15,33 +16,70 @@ import android.view.MenuItem;
 
 import com.example.reddittopviewer.databinding.ActivityScrollingBinding;
 
+import org.jetbrains.annotations.Contract;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class ScrollingActivity extends AppCompatActivity {
-
-    private ActivityScrollingBinding binding;
-
-    Reddit_top Top;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        com.example.reddittopviewer.databinding.ActivityScrollingBinding binding = ActivityScrollingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
-        Top.JSONFetch("https://www.reddit.com/top.json");
-        try {
-            Top.JSONChildren("children");
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-        Top.ShowPosts(this);
+
+        new Thread(new Runnable() {
+            public void run(){
+                //txt1.setText("Thread!!");
+            }
+            public void main(String[] args){
+                URL Reddit_top;
+                try {
+                    Reddit_top = new URL("https://www.reddit.com/top.json");
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+                JSONObject json  = JSONFetch(Reddit_top);
+                System.out.println(json);
+            }
+            @NonNull
+            private String readAll(@NonNull Reader rd) throws IOException {
+                StringBuilder sb = new StringBuilder();
+                int cp;
+                while ((cp = rd.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                return sb.toString();
+            }
+
+            @NonNull
+            @Contract("_ -> new")
+            public JSONObject JSONFetch(URL url)//Imports the JSON file and fetches the data from it
+            {
+                try (InputStream is = url.openStream()) {
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+                    String jsonText = readAll(rd);
+                    return new JSONObject(jsonText);
+                } catch (IOException | JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -52,7 +90,7 @@ public class ScrollingActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
